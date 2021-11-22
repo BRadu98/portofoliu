1c57b2740c597de98052154879ae87ef2e238efb
Baloi Radu <bradu2020@gmail.com>
2021-11-22 19:48:15 +0200

    next img

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,31 @@
 import HomeStyles from '../components/styles/HomeStyles'
+import Image from 'next/image'
+import coding from '../public/coding.png'
 import Container from './styles/ContainerStyles';
-
 
 export default function Home() {
   
   return (
     <HomeStyles>
-      <Container>H</Container>
+      <Container>
+        <div className="fx-row">
+          <div className="w50">
+            <div>
+              <span className="intro">Hey, I'm Radu</span>
+              <span className="descr">An Junior Web Developer with a can-do attitude.</span>
+            </div>
+          </div>
+          <div className="w50">
+            <div>
+              <Image
+        src={coding}
+        alt="coding"
+        // layout='responsive'
+      />
+            </div>
+          </div>
+        </div>
+      </Container>
     </HomeStyles>
   );
 }
--- components/styles/HomeStyles.js
+++ components/styles/HomeStyles.js
@@ -2,10 +2,28 @@ import styled from 'styled-components';
 
 const HomeStyles = styled.section`
   background: linear-gradient(to right, #24243e, #302b63, #0f0c29);
-  padding: 10rem 0 0 0;
+  padding: 6rem 0 0 0; //10rem
   width: 100vw;
   height: 100%;
   color: #fff;
+
+  .fx-row {
+    display: flex;
+    gap: 1rem;
+  }
+  .w50 {
+    min-width: 340px;
+  }
+  .intro {
+    font-size: 2rem;
+    display: inline-block;
+    font-weight: 700;
+  }
+  .descr {
+    display: block;
+    margin-top: 1.2rem;
+    font-size: 1.5rem;
+  }
 `;
 
 export default HomeStyles
