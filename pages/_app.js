4d4d47b2d2d2684fc4836f337ff3c78e6dc368b9
Baloi Radu <bradu2020@gmail.com>
2021-11-28 00:38:48 +0200

    useContext, useWindowDimensions

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import Page from "../components/Page"
 import Script from 'next/script'
+import {SSRProvider} from '@react-aria/ssr'; //!Fix class name diff
+import { NavStateProvider } from '../lib/navState'; //!UseContext
+
+
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Page>
-      <Script src="https://kit.fontawesome.com/0bae28e39f.js" crossOrigin="anonymous"></Script> 
-      <Component {...pageProps} />
-    </Page>
+    <SSRProvider>
+      <NavStateProvider>
+        <Page>
+        <Script src="https://kit.fontawesome.com/0bae28e39f.js" crossOrigin="anonymous"></Script> 
+        <Component {...pageProps} />
+      </Page>
+      </NavStateProvider>
+    </SSRProvider>
+
   )
 }
 
