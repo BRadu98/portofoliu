b024e81b319815204b1150b40b81bacea46e6103
Baloi Radu <bradu2020@gmail.com>
2021-12-02 06:48:30 +0200

    headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
-const withPWA = require("next-pwa")
+const withPWA = require('next-pwa')
 const runtimeCaching = require("next-pwa/cache")
+
+const headers = async () => {
+  return [
+  {
+    source: '/(.*)',
+    headers: [
+    {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff'
+    },
+    {
+      key: 'X-Frame-Options',
+      value: 'SAMEORIGIN'
+    },
+    {
+      key: 'X-XSS-Protection',
+      value: '1; mode=block'
+    }
+  ],
+  },
+  ]
+}
 
 module.exports = withPWA({
   reactStrictMode: true,
@@ -9,6 +31,7 @@ module.exports = withPWA({
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
     runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/]
-  }
+    buildExcludes: [/middleware-manifest.json$/],
+  },
+  headers
 })
